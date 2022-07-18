9960b5905ab5127a4e57e6e8405c7510c9c48391
TommyGymer <tommygymer@gmail.com>
2022-07-18 20:31:13 +0100

    Added resource links

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+//make use of https://crates.io/crates/image to decode main image formats
+//make use of https://github.com/pedrocr/rawloader/ for decoding of RAW images
+//make use of https://docs.rs/druid/0.7.0/druid/widget/struct.Image.html the druid image widget for displaying images
+
 fn main() {
     println!("Hello, world!");
 }
