5c652557fc9ffa6732c52002289fefd3b34c8b8b
TommyGymer <tommygymer@gmail.com>
2022-09-06 12:19:29 +0100

    Draw triangle

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,35 @@
 
 use beryllium::*;
 use ogl33::*;
+use core::{
+    convert::{TryInto},
+    mem::{size_of, size_of_val},
+  };
+
+//triangle data
+type Vertex = [f32; 3];
+const TRIANGLE: [Vertex; 3] =
+    [
+        [-0.5, -0.5, 0.0],
+        [0.5, -0.5, 0.0],
+        [0.0, 0.5, 0.0]
+    ];
+
+//vertex shader program
+const VERT_SHADER: &str = r#"#version 330 core
+  layout (location = 0) in vec3 pos;
+  void main() {
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+  }
+"#;
+
+const FRAG_SHADER: &str = r#"#version 330 core
+  out vec4 final_colour;
+
+  void main() {
+    final_colour = vec4(1.0, 0.5, 0.2, 1.0);
+  }
+"#;
 
 fn main() {
     let sdl = SDL::init(InitFlags::Everything).expect("couldn't start SDL");
@@ -31,7 +60,131 @@ fn main() {
     
     unsafe {
         load_gl_with(|f_name| win.get_proc_address(f_name));
+
+        glClearColor(0.2, 0.3, 0.3, 1.0);
+
+        //vertex array
+        let mut vao = 0;
+        glGenVertexArrays(1, &mut vao);
+        assert_ne!(vao, 0);
+
+        //vertex buffer
+        let mut vbo = 0;
+        glGenBuffers(1, &mut vbo);
+        assert_ne!(vbo, 0);
+
+        //bindings
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+        //buffering triangle data
+        glBufferData(
+            GL_ARRAY_BUFFER,
+            size_of_val(&TRIANGLE) as isize,
+            TRIANGLE.as_ptr().cast(),
+            GL_STATIC_DRAW,
+        );
+
+        //define the data as vertex data
+        glVertexAttribPointer(
+            0,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            size_of::<Vertex>().try_into().unwrap(),
+            0 as *const _,
+        );
+        glEnableVertexAttribArray(0);
+
+        //vertex shader
+        let vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+        assert_ne!(vertex_shader, 0);
+
+        //compile vertex shader program
+        glShaderSource(
+            vertex_shader,
+            1,
+            &(VERT_SHADER.as_bytes().as_ptr().cast()),
+            &(VERT_SHADER.len().try_into().unwrap()),
+        );
+        glCompileShader(vertex_shader);
+
+        //check compile status
+        let mut success = 0;
+        glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &mut success);
+        if success == 0 {
+            let mut v: Vec<u8> = Vec::with_capacity(1024);
+            let mut log_len = 0_i32;
+            glGetShaderInfoLog(
+                vertex_shader,
+                1024,
+                &mut log_len,
+                v.as_mut_ptr().cast(),
+            );
+            v.set_len(log_len.try_into().unwrap());
+            panic!("Vertex Compile Error: {}", String::from_utf8_lossy(&v));
+        }
+
+        //fragment shader
+        let fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+        assert_ne!(fragment_shader, 0);
+
+        //compile vertex shader program
+        glShaderSource(
+            fragment_shader,
+            1,
+            &(FRAG_SHADER.as_bytes().as_ptr().cast()),
+            &(FRAG_SHADER.len().try_into().unwrap()),
+        );
+        glCompileShader(fragment_shader);
+
+        //check compile status
+        let mut success = 0;
+        glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &mut success);
+        if success == 0 {
+            let mut v: Vec<u8> = Vec::with_capacity(1024);
+            let mut log_len = 0_i32;
+            glGetShaderInfoLog(
+                fragment_shader,
+                1024,
+                &mut log_len,
+                v.as_mut_ptr().cast(),
+            );
+            v.set_len(log_len.try_into().unwrap());
+            panic!("Vertex Compile Error: {}", String::from_utf8_lossy(&v));
+        }
+
+        //shader program
+        let shader_program = glCreateProgram();
+        glAttachShader(shader_program, vertex_shader);
+        glAttachShader(shader_program, fragment_shader);
+        glLinkProgram(shader_program);
+
+        //check link success
+        let mut success = 0;
+        glGetProgramiv(shader_program, GL_LINK_STATUS, &mut success);
+        if success == 0 {
+            let mut v: Vec<u8> = Vec::with_capacity(1024);
+            let mut log_len = 0_i32;
+            glGetProgramInfoLog(
+                shader_program,
+                1024,
+                &mut log_len,
+                v.as_mut_ptr().cast(),
+            );
+            v.set_len(log_len.try_into().unwrap());
+            panic!("Program Link Error: {}", String::from_utf8_lossy(&v));
+        }
+
+        //delete vertex and fragment shaders
+        glDeleteShader(vertex_shader);
+        glDeleteShader(fragment_shader);
+
+        //set the program as the program to use
+        glUseProgram(shader_program);
     }
+
+    win.set_swap_interval(SwapInterval::Vsync);
 
     'main_loop: loop {
         // handle events this frame
@@ -41,8 +194,12 @@ fn main() {
             _ => (),
             }
         }
-        // now the events are clear
-    
-        // here's where we could change the world state and draw.
+
+        unsafe {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        }
+
+        win.swap_window();
     }
 }
