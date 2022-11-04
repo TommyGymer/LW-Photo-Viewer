c0119db382aacb82115af40a9f29c5b961b7f244
TommyGymer <tommygymer@gmail.com>
2022-11-04 16:21:49 +0000

    Added specialised libs for jpg, png and qoi

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@
 use eframe::egui;
 use egui_extras::RetainedImage;
 use egui::Vec2;
+use image::{ImageBuffer, Rgba};
+use qoi::{decode_to_buf, decode_to_vec};
 use std::{fs, env};
 use std::path::Path;
 use std::time::Instant;
+use std::io::Cursor;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -46,46 +49,66 @@ impl Default for LwPv {
 fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
     let mut start = Instant::now();
 
-    let file = image::io::Reader::open(path)?;
+    let file: &[u8] = &(fs::read(path).unwrap());
 
     println!("File read: {:?}", start.elapsed());
     start = Instant::now();
 
-    let image = match path.extension() {
-        Some(ext) => file.decode()?,
-        None => file.decode()?,
+    let image_buffer: ImageBuffer<Rgba<u8>, Vec<u8>> = match path.extension() {
+        Some(ext) => {
+            println!("{:?}", ext.to_str().unwrap());
+            match ext.to_str().unwrap() {
+                "jpg" => {
+                    let tmp: image::RgbaImage = turbojpeg::decompress_image(file).expect("could not read jpg");
+                    tmp
+                },
+                "png" => {
+                    let cursor = Cursor::new(file);
+                    let decoder = spng::Decoder::new(cursor);
+                    let (info, mut reader) = decoder.read_info().expect("could not read info");
+                    let mut out: Vec<u8> = vec![0; reader.output_buffer_size()];
+                    reader.next_frame(&mut out).expect("could not decode image");
+
+                    image::ImageBuffer::from_raw(
+                        info.width as u32,
+                        info.height as u32,
+                        out,
+                    ).expect("could not find frame")
+                }
+                "qoi" => {
+                    let (header, decoded) = decode_to_vec(file).expect("could not read qoi");
+                    image::ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(
+                        header.width as u32,
+                        header.height as u32,
+                        decoded,
+                    ).expect("could not read qoi")
+                }
+                _ => image::load_from_memory(file)?.to_rgba8(),
+            }
+        },
+        None => image::load_from_memory(file)?.to_rgba8(),
     };
     
-
     println!("Image decoded: {:?}", start.elapsed());
     start = Instant::now();
 
-    let size = [image.width() as _, image.height() as _];
-    let image_buffer = image.to_rgba8();
-
-    println!("To rgba: {:?}", start.elapsed());
-    start = Instant::now();
+    let size = [image_buffer.width() as _, image_buffer.height() as _];
 
     let pixels = image_buffer.as_flat_samples();
 
     println!("To samples: {:?}", start.elapsed());
+    start = Instant::now();
 
-    Ok(egui::ColorImage::from_rgba_unmultiplied(
+    //consumes 4 bytes at a time
+    let result = Ok(egui::ColorImage::from_rgba_unmultiplied(
         size,
         pixels.as_slice(),
-    ))
-}
+    ));
 
-// fn load_image_from_memory(image_data: &[u8]) -> Result<ColorImage, image::ImageError> {
-//     let image = image::load_from_memory(image_data)?;
-//     let size = [image.width() as _, image.height() as _];
-//     let image_buffer = image.to_rgba8();
-//     let pixels = image_buffer.as_flat_samples();
-//     Ok(ColorImage::from_rgba_unmultiplied(
-//         size,
-//         pixels.as_slice(),
-//     ))
-// }
+    println!("To egui image: {:?}", start.elapsed());
+
+    result
+}
 
 impl eframe::App for LwPv {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
