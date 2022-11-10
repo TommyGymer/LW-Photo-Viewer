3f7d44f3fd2a64cb5c59d600bc443d883ee58a26
TommyGymer <tommygymer@gmail.com>
2022-11-10 15:12:26 +0000

    Code clean up

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@
 // use image::io::Reader as ImageReader;
 use eframe::egui;
 use eframe::epaint::Color32;
-use egui_extras::RetainedImage;
 use egui::Vec2;
+use egui_extras::RetainedImage;
 use image::error::{DecodingError, ImageFormatHint};
-use image::{Rgba, Rgb};
+use image::{Rgb, Rgba};
 use qoi::decode_to_vec;
 use rayon::iter::ParallelIterator;
 use rayon::slice::ParallelSlice;
 use std::ffi::OsString;
-use std::{fs, env};
+use std::io::Cursor;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
-use std::io::Cursor;
+use std::{env, fs};
 use windows::Win32::UI::Shell::IThumbnailCache;
 use windows::Win32::UI::Shell::PropertiesSystem::IInitializeWithStream;
 // use notify_rust::Notification;
@@ -38,7 +38,7 @@ fn main() {
         initial_window_size: Some(egui::vec2(1024.0, 900.0)),
         ..Default::default()
     };
-    
+
     eframe::run_native(
         "LW Photo Viewer",
         options,
@@ -59,12 +59,8 @@ impl Default for LwPv {
                 "image",
                 load_image_from_path(Path::new("./img/no_image.png")).unwrap(),
             ),
-            image_path: String::from(
-                "./img/no_image.png"
-            ),
-            folder: String::from(
-                "./img/"
-            ),
+            image_path: String::from("./img/no_image.png"),
+            folder: String::from("./img/"),
         }
     }
 }
@@ -89,85 +85,55 @@ fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, imag
     let file: &[u8] = &(fs::read(path).unwrap());
 
     let result = match path.extension() {
-        Some(ext) => {
-            match ext.to_str().unwrap() {
-                "jpg" => {
-                    let tmp: image::RgbImage = decode_jpg(file, path.to_path_buf())?;
-                    Ok(color_image_from_rgb_image_buffer(tmp)?)
-                },
-                "jfif" => {
-                    let tmp: image::RgbImage = decode_jpg(file, path.to_path_buf())?;
-                    Ok(color_image_from_rgb_image_buffer(tmp)?)
-                }
-                "png" => {
-                    let cursor = Cursor::new(file);
-                    let decoder = spng::Decoder::new(cursor);
-                    let (info, mut reader) = decoder.read_info().expect("could not read info");
-                    
-                    let mut out: Vec<u8> = vec![0; reader.output_buffer_size()];
-                    reader.next_frame(&mut out).unwrap();
-                    
-                    match info.color_type {
-                        spng::ColorType::Truecolor => {
-                            color_image_from_rgb_image_buffer(
-                                rgb_image_from_raw(
-                                    info.width,
-                                    info.height,
-                                    out,
-                                    path.to_path_buf(),
-                                )?
-                            )
-                        },
-                        spng::ColorType::TruecolorAlpha => {
-                            color_image_from_rgba_image_buffer(
-                                rgba_image_from_raw(
-                                    info.width,
-                                    info.height,
-                                    out,
-                                    path.to_path_buf(),
-                                )?
-                            )
-                        },
-                        _ => panic!("not implemented grayscale png"),
-                    }
-                }
-                "qoi" => {
-                    let (header, decoded) = decode_to_vec(file).expect("could not read qoi");
-                    match header.channels {
-                        qoi::Channels::Rgb => {
-                            color_image_from_rgb_image_buffer(
-                                rgb_image_from_raw(
-                                    header.width,
-                                    header.height,
-                                    decoded,
-                                    path.to_path_buf(),
-                                )?
-                            )
-                        },
-                        qoi::Channels::Rgba => {
-                            color_image_from_rgba_image_buffer(
-                                rgba_image_from_raw(
-                                    header.width,
-                                    header.height,
-                                    decoded,
-                                    path.to_path_buf(),
-                                )?
-                            )
-                        },
-                    }
-                }
-                _ => color_image_from_rgba_image_buffer(
-                        image::load_from_memory(file)?
-                            .to_rgba8()
+        Some(ext) => match ext.to_str().unwrap() {
+            "jpg" => Ok(color_image_from_rgb_image_buffer(decode_jpg(
+                file,
+                path.to_path_buf(),
+            )?)?),
+            "jfif" => Ok(color_image_from_rgb_image_buffer(decode_jpg(
+                file,
+                path.to_path_buf(),
+            )?)?),
+            "png" => {
+                let cursor = Cursor::new(file);
+                let decoder = spng::Decoder::new(cursor);
+                let (info, mut reader) = decoder.read_info().expect("could not read info");
+
+                let mut out: Vec<u8> = vec![0; reader.output_buffer_size()];
+                reader.next_frame(&mut out).unwrap();
+
+                match info.color_type {
+                    spng::ColorType::Truecolor => color_image_from_rgb_image_buffer(
+                        rgb_image_from_raw(info.width, info.height, out, path.to_path_buf())?,
                     ),
+                    spng::ColorType::TruecolorAlpha => color_image_from_rgba_image_buffer(
+                        rgba_image_from_raw(info.width, info.height, out, path.to_path_buf())?,
+                    ),
+                    _ => panic!("not implemented grayscale png"),
+                }
             }
+            "qoi" => {
+                let (header, decoded) = decode_to_vec(file).expect("could not read qoi");
+                match header.channels {
+                    qoi::Channels::Rgb => color_image_from_rgb_image_buffer(rgb_image_from_raw(
+                        header.width,
+                        header.height,
+                        decoded,
+                        path.to_path_buf(),
+                    )?),
+                    qoi::Channels::Rgba => color_image_from_rgba_image_buffer(rgba_image_from_raw(
+                        header.width,
+                        header.height,
+                        decoded,
+                        path.to_path_buf(),
+                    )?),
+                }
+            }
+            _ => color_image_from_rgba_image_buffer(image::load_from_memory(file)?.to_rgba8()),
         },
-        None => Ok(
-            color_image_from_rgba_image_buffer(
-                image::load_from_memory(file)?
-                    .to_rgba8()
-            )?
-        ),
+        None => Ok(color_image_from_rgba_image_buffer(
+            image::load_from_memory(file)?.to_rgba8(),
+        )?),
     };
     println!("Total: {:?}", start.elapsed());
     result
@@ -176,33 +142,44 @@ fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, imag
 fn decode_jpg(file: &[u8], ext: PathBuf) -> Result<image::RgbImage, image::ImageError> {
     match turbojpeg::decompress_image(file) {
         Ok(tmp) => Ok(tmp),
-        Err(err) => Err(image::ImageError::Decoding(DecodingError::new(ImageFormatHint::PathExtension(ext), err))),
+        Err(err) => Err(image::ImageError::Decoding(DecodingError::new(
+            ImageFormatHint::PathExtension(ext),
+            err,
+        ))),
     }
 }
 
-fn rgb_image_from_raw(width: u32, height: u32, data: Vec<u8>, ext: PathBuf) -> Result<image::RgbImage, image::ImageError> {
+fn rgb_image_from_raw(
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+    ext: PathBuf,
+) -> Result<image::RgbImage, image::ImageError> {
     let start = Instant::now();
-    let result = match image::ImageBuffer::<Rgb<u8>, Vec<u8>>::from_raw(
-        width,
-        height,
-        data,
-    ) {
+    let result = match image::ImageBuffer::<Rgb<u8>, Vec<u8>>::from_raw(width, height, data) {
         Some(image) => Ok(image),
-        None => Err(image::ImageError::Decoding(DecodingError::new(ImageFormatHint::PathExtension(ext), "Raw image bytes did not fit the image container"))),
+        None => Err(image::ImageError::Decoding(DecodingError::new(
+            ImageFormatHint::PathExtension(ext),
+            "Raw image bytes did not fit the image container",
+        ))),
     };
     println!("RGB image from raw: {:?}", start.elapsed());
     result
 }
 
-fn rgba_image_from_raw(width: u32, height: u32, data: Vec<u8>, ext: PathBuf) -> Result<image::RgbaImage, image::ImageError> {
+fn rgba_image_from_raw(
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+    ext: PathBuf,
+) -> Result<image::RgbaImage, image::ImageError> {
     let start = Instant::now();
-    let result = match image::ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(
-        width,
-        height,
-        data,
-    ) {
+    let result = match image::ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(width, height, data) {
         Some(image) => Ok(image),
-        None => Err(image::ImageError::Decoding(DecodingError::new(ImageFormatHint::PathExtension(ext), "Raw image bytes did not fit the image container"))),
+        None => Err(image::ImageError::Decoding(DecodingError::new(
+            ImageFormatHint::PathExtension(ext),
+            "Raw image bytes did not fit the image container",
+        ))),
     };
     println!("RGBA image from raw: {:?}", start.elapsed());
     result
@@ -228,26 +205,24 @@ fn from_rgba_unmultiplied(size: [usize; 2], rgba: &[u8]) -> egui::ColorImage {
     egui::ColorImage { size, pixels }
 }
 
-fn color_image_from_rgb_image_buffer(image: image::RgbImage) -> Result<egui::ColorImage, image::ImageError>{
+fn color_image_from_rgb_image_buffer(
+    image: image::RgbImage,
+) -> Result<egui::ColorImage, image::ImageError> {
     let start = Instant::now();
     let size = [image.width() as _, image.height() as _];
     let pixels = image.as_flat_samples();
-    let result = Ok(from_rgb_unmultiplied(
-        size,
-        pixels.as_slice(),
-    ));
+    let result = Ok(from_rgb_unmultiplied(size, pixels.as_slice()));
     println!("egui RGB ColorImage: {:?}", start.elapsed());
     result
 }
 
-fn color_image_from_rgba_image_buffer(image: image::RgbaImage) -> Result<egui::ColorImage, image::ImageError>{
+fn color_image_from_rgba_image_buffer(
+    image: image::RgbaImage,
+) -> Result<egui::ColorImage, image::ImageError> {
     let start = Instant::now();
     let size = [image.width() as _, image.height() as _];
     let pixels = image.as_flat_samples();
-    let result = Ok(from_rgba_unmultiplied(
-        size,
-        pixels.as_slice(),
-    ));
+    let result = Ok(from_rgba_unmultiplied(size, pixels.as_slice()));
     println!("egui RGBA ColorImage: {:?}", start.elapsed());
     result
 }
@@ -257,32 +232,34 @@ impl eframe::App for LwPv {
         _frame.set_window_title(&self.image_path);
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.centered_and_justified(|ui| {
-                ui.add(
-                    egui::Image::new(
-                        self.image.texture_id(ctx),
-                        max_maintain_ar(ui.available_size(), self.image.size_vec2()),
-                    )
-                );
+                ui.add(egui::Image::new(
+                    self.image.texture_id(ctx),
+                    max_maintain_ar(ui.available_size(), self.image.size_vec2()),
+                ));
             });
             if ui.input().key_pressed(egui::Key::ArrowRight) {
                 match fs::read_dir(Path::new(&self.folder)) {
                     Err(why) => println!("! {:?}", why.kind()),
                     Ok(paths) => {
                         let mut found = false;
-                        let vec_paths: Vec<fs::DirEntry> = paths.collect::<Result<Vec<_>, _>>().unwrap();
+                        let vec_paths: Vec<fs::DirEntry> =
+                            paths.collect::<Result<Vec<_>, _>>().unwrap();
                         for path in vec_paths {
                             if found {
-                                self.image_path = path.path().as_path().to_str().unwrap().to_string();
+                                self.image_path = path
+                                    .path()
+                                    .as_path()
+                                    .to_str()
+                                    .unwrap()
+                                    .to_string();
                                 self.image = RetainedImage::from_color_image(
                                     "image",
-                                    match load_image_from_path(
-                                        path.path().as_path()
-                                    ) {
+                                    match load_image_from_path(path.path().as_path()) {
                                         Ok(image) => image,
                                         Err(err) => {
                                             println!("{:?}", err);
                                             break;
-                                        },
+                                        }
                                     },
                                 );
                                 break;
@@ -290,7 +267,7 @@ impl eframe::App for LwPv {
                                 found = true;
                             }
                         }
-                    },
+                    }
                 }
             }
             if ui.input().key_pressed(egui::Key::ArrowLeft) {
@@ -298,21 +275,24 @@ impl eframe::App for LwPv {
                     Err(why) => println!("! {:?}", why.kind()),
                     Ok(paths) => {
                         let mut found = false;
-                        let vec_paths: Vec<fs::DirEntry> = paths.collect::<Result<Vec<_>, _>>().unwrap();
+                        let vec_paths: Vec<fs::DirEntry> =
+                            paths.collect::<Result<Vec<_>, _>>().unwrap();
                         for i in 1..=vec_paths.len() {
                             let path = vec_paths[vec_paths.len() - i].path();
                             if found {
-                                self.image_path = path.as_path().to_str().unwrap().to_string();
+                                self.image_path = path
+                                    .as_path()
+                                    .to_str()
+                                    .unwrap()
+                                    .to_string();
                                 self.image = RetainedImage::from_color_image(
                                     "image",
-                                    match load_image_from_path(
-                                        path.as_path()
-                                    ) {
+                                    match load_image_from_path(path.as_path()) {
                                         Ok(image) => image,
                                         Err(err) => {
                                             println!("{:?}", err);
                                             break;
-                                        },
+                                        }
                                     },
                                 );
                                 break;
@@ -320,7 +300,7 @@ impl eframe::App for LwPv {
                                 found = true;
                             }
                         }
-                    },
+                    }
                 }
             }
         });
